389e7521a4d407ca6fcc094db8b44d4e08f951ac
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2024-09-26 10:30:54 +0200

    Retrieves symptoms and accounts + read symptoms.txt

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,30 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 
-	public static void main(String[] args) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("../symptoms.txt"));
-		String line = reader.readLine();
+	private static Map<String, Integer> symptomsCount = new HashMap<>();
 
-		int i = 0;	// set i to 0
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	public static void main(String[] args) throws IOException {
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = symptomReader.GetSymptoms();
 
-			line = reader.readLine();	// get another symptom
+		// Retrieves symptoms and accounts
+		for (String symptom : symptoms) {
+			symptomsCount.put(symptom, symptomsCount.getOrDefault(symptom, 0) + 1);
 		}
 
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dilated pupils: " + pupilCount + "\n");
+		for (String symptom : symptomsCount.keySet()) {
+			writer.write(symptom + "\t" + symptomsCount.get(symptom) + "\n");
+		}
 		writer.close();
 	}
 }
